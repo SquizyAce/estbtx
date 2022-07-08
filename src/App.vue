1ddcffb9c6a95f8a5a5eb050f42209767429fa93
squizyace <squizyace@gmail.com>
2022-07-09 01:30:05 +0700

    callback sync

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,141 @@
 <template>
   <div id="app">
-    <nav>
-    </nav>
+<div>
+  <b-navbar type="dark" variant="dark">
+    <b-navbar-nav>
+      <b-nav-item href="#" @click="showModal">Добавить</b-nav-item>
+    </b-navbar-nav>
+  </b-navbar>
+</div>
+
+
     <b-table striped hover :items="clearTh" :fields="clearField"></b-table>
+
+  <div>
+    <b-modal ref="my-modal" size="lg" hide-footer title="Using Component Methods">
+      <div class="d-block text-center">
+
+        <b-form-group label="Тип расхода" label-for="name-input">
+          <b-form-select v-model="selected" :options="options"></b-form-select>
+        </b-form-group>
+
+        <b-form-group label="Назначения платежа" label-for="name-input">
+          <b-form-input v-model="payname" id="text" type="text"></b-form-input>
+        </b-form-group>
+
+        <b-form-group label="Планируемая дата оплаты" label-for="name-input">
+          <b-form-input v-model="trueDate" id="date" type="date"></b-form-input>
+        </b-form-group>
+
+        <b-form-group label="Фактическая дата оплаты" label-for="name-input">
+          <b-form-input v-model="realDate" id="date" type="date"></b-form-input>
+        </b-form-group>
+
+        <b-form-group label="Сумма расхода" label-for="name-input">
+          <b-form-input v-model="payment" id="number" type="number"></b-form-input>
+        </b-form-group>
+
+        <b-form-group label="Поле связь со сделкой" label-for="name-input">
+          <b-form-select v-model="selected" :options="options"></b-form-select>
+        </b-form-group>
+
+        <b-form-group label="Ответственный " label-for="name-input">
+          <b-form-select v-model="selected" :options="options"></b-form-select>
+        </b-form-group>
+
+        
+
+      </div>
+      <b-button class="mt-3" variant="outline-info" block @click.prevent="elemAd()">Добавить</b-button>
+    </b-modal>
   </div>
+
+
+  </div>
+  
 </template>
 
 <script>
+import { v4 as uuidv4 } from 'uuid';
 export default {
   data: () => ({
     clearTh: {},
-    clearField: []
+    clearField: [],
+    selected: null,
+    payname: '',
+    trueDate: '',
+    realDate: '',
+    payment: null,
+        options: [
+          { value: null, text: 'Please select an option' },
+          { value: 'a', text: 'This is First option' },
+          { value: 'b', text: 'Selected Option' },
+          { value: { C: '3PO' }, text: 'This is an option with object value' },
+          { value: 'd', text: 'This one is disabled', disabled: true }
+        ]
   }),
   async mounted() {
     console.log(BX24.getAuth())
-    await this.$store.dispatch('fetchItems')
     await this.$store.dispatch('fetchFields')
-    
+    let params = {
+                'IBLOCK_TYPE_ID': 'lists',
+                'IBLOCK_ID': '33',
+            }
+            BX24.callMethod(
+                'lists.field.get',
+                params,
+                function(result)
+                {
+                    if(result.error())
+                        alert("Error: " + result.error());
+                    else{
+                        console.log(result.data())
+                    }
+                }
+            );
+  },
+  methods: {
+    showModal() {
+        this.$refs['my-modal'].show()
+      },
+    elemAd() {
+      const formData = {
+        payname: this.payname,
+        trueDate: this.trueDate,  
+        realDate: this.realDate,
+        payment: this.payment    
+      }
+      console.log(formData)
+
+      var params = {
+      'IBLOCK_TYPE_ID': 'lists',
+      'IBLOCK_ID': '33',
+      'ELEMENT_CODE': uuidv4(),
+      'FIELDS': {
+          'NAME': 'Test element 1',
+          // 'PROPERTY_123': { 'n0': },
+          'PROPERTY_125': { 'n0': this.payname },
+          'PROPERTY_127': { 'n0': this.trueDate },
+          'PROPERTY_129': { 'n0': this.realDate },
+          'PROPERTY_131': { 'n0': this.payment },
+          'PROPERTY_133': { 'n0': 1 },
+          'PROPERTY_135': { 'n0': 7 },
+          
+      }
+      }      
+      BX24.callMethod(
+      'lists.element.add',
+      params,
+      function(result)
+      {
+            if(result.error())
+              alert("Error: " + result.error());
+          else
+              console.log("Success: " + result.data());
+      }
+      );      
+      this.$router.go()
+    }
   },
   computed: {
     items() {
@@ -29,12 +148,19 @@ export default {
   watch: {
 
     items() {
-      console.log(this.items)
       this.clearTh = this.items.map(function(sort) {
         let clear = []
         for (const [key, value] of Object.entries(sort)) {
         if (key.includes('PROPERTY_') || key === 'ID')
-        clear[key] = value
+        {
+          if (typeof value === 'object')
+          for (const [key1, value1] of Object.entries(value)) {
+            sort[key] = value1
+            
+          }
+          clear[key] = sort[key]
+        }
+        
       }
       return clear
       })
--- src/store/modules/list.js
+++ src/store/modules/list.js
@@ -1,22 +1,92 @@
 export default {
     state: {
         items: [],
-        fields: {}
+        fields: {},
+        users: [],
+        deals: []
     },
     mutations:{
         setItems(state, items) {
             state.items = items
         },
+        setUsers(state, users) {
+            state.users = users
+        },
         setFields(state, fields) {
             state.fields = fields
+        },
+        setDeals(state, deals) {
+            state.deals = deals
         },
         clearInfo(state) {
             state.items = []
             state.fields = {}
+            state.users = []
+            state.deals = []
         }
     },
     actions: {
-        fetchItems({commit}){
+
+        fetchFields({commit, dispatch}){ // у нас калбэк функции по-этому сначало загружаем титульные поля
+            let params = {
+                'IBLOCK_TYPE_ID': 'lists',
+                'IBLOCK_ID': '33',
+            }
+            BX24.callMethod(
+                'lists.field.get',
+                params,
+                function(result)
+                {
+                    if(result.error())
+                        alert("Error: " + result.error());
+                    else{
+                        let fields = result.data()
+                        commit('setFields', fields)
+                        dispatch('fetchUsers')  // загружаем пользователей
+                    }
+                }
+            );
+        },
+
+        fetchUsers({commit, dispatch}){ 
+            BX24.callMethod(
+                'user.get',
+                "ASC",
+                function(result)
+                {
+                    if(result.error())
+                        alert("Error: " + result.error());
+                    else{
+                        let users = result.data()
+                        commit('setUsers', users)
+                        dispatch('fetchDeals')
+                    }
+                }
+            );
+        },
+
+        fetchDeals({commit, dispatch}){
+            BX24.callMethod(
+                "crm.deal.list", 
+                { 
+                    select: [ "ID", "TITLE" ]
+                }, 
+                function(result) 
+                {
+                    if(result.error())
+                        console.log(result.error());
+                    else
+                    {
+
+                        let deals = result.data()
+                        commit('setDeals', deals)			
+                        dispatch('fetchItems')					
+                    }
+                }
+            );	
+        },
+
+        fetchItems({commit}){ // калбек на ячейки
             let params = {
                 'IBLOCK_TYPE_ID': 'lists',
                 'IBLOCK_ID': '33',
@@ -34,29 +104,12 @@ export default {
                     }
                 }
             );
-        },
-        fetchFields({commit}){
-            let params = {
-                'IBLOCK_TYPE_ID': 'lists',
-                'IBLOCK_ID': '33',
-            }
-            BX24.callMethod(
-                'lists.field.get',
-                params,
-                function(result)
-                {
-                    if(result.error())
-                        alert("Error: " + result.error());
-                    else{
-                        let fields = result.data()
-                        commit('setFields', fields)
-                    }
-                }
-            );
-        },
+        },      
     },
     getters: {
+        deals: s => s.deals,
+        users: s => s.users,
         items: s => s.items,
-        fields: s => s.fields
+        fields: s => s.fields,
     }
 }
